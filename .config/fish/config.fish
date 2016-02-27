bebfee0e782b6a935e6f6222271efbfb1dfd4a52
Michael Palmos <toqoz@hotmail.com>
2016-02-27 14:41:05 +1000

    ...

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,11 @@
-set fish_greeting " "
-eval (dircolors -c ~/.dircolors | sed 's/>&\/dev\/null$//')
+if status --is-interactive
+
+    set fish_greeting " "
+    eval (dircolors -c ~/.dircolors | sed 's/>&\/dev\/null$//')
+
+    if test -z (echo $TMUX)
+        if not test (~/git/tmx 0)
+            tmux new-session
+        end
+    end
+end
